ed9f547848057fa0af4287794bf2c57220b46af6
paizhang <zhangpai91@gmail.com>
2017-07-01 22:11:50 -0700

    finish assignment 1

--- Assignments/Lesson01-MongoDB-Connection/student-start/assignment.rb
+++ Assignments/Lesson01-MongoDB-Connection/student-start/assignment.rb
@@ -1,18 +1,22 @@
 require 'pp'
+require 'mongo'
 
 class Solution
-  @@db = nil
+  @@db = nil  # Class variable
   
   #Implement a class method in the `Solution` class called `mongo_client` that will 
   def self.mongo_client
     #create a `Mongo::Client` connection to the server using a URL (.e.g., 'mongodb://localhost:27017')
     #configure the client to use the `test` database
     #assign the client to @@db class variable and return that client
-    
+    @@db = Mongo::Client.new('mongodb://localhost:27017');
+    @@db = @@db.use('test');
+
   end
 
   #Implement a class method in the `Solution` class called `collection` that will
   def self.collection
+    @@db[:zips];
     #return the `zips` collection
     
   end
@@ -21,7 +25,7 @@ class Solution
   def sample
     #return a single document from the `zips` collection from the database. 
     #This does not have to be random. It can be first, last, or any other document in the collection.
-
+    @@db[:zips].find.first;
   end
 end
 
